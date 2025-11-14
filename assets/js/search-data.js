6fc86078c83aafd94af513c609c0261f5864d9f3
pcallec <pcallec@outlook.com>
2025-11-14 23:17:48 +0000

    Deploying to gh-pages from @ MillerLabOU/millerlabou.github.io@cac997c5bba5979379c776cf9b0066df6b3d6e67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "members of the lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-real-time-assessment-of-blood-flow-imaging-during-neurosurgery",
+          title: 'Real-time assessment of blood flow imaging during neurosurgery',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
